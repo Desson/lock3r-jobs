a0ec173b6e997eab9b7a5351ecf32cd70611949e
jacobdalegrieh <54174993+jacobdalegrieh@users.noreply.github.com>
2020-11-26 22:41:40 +0000

    lock3r-jobs(more on 27/11/2020)

---
+++ abis/lock3runitrade.js
@@ -0,0 +1,8 @@
+const config = {
+    address: "", //not ready
+    abi: [
+        "function work (  ) external",
+        "function workable (  ) external view returns ( bool )",
+    ],
+};
+module.exports = config;
---
+++ config/wallet.js
@@ -0,0 +1,9 @@
+const ethers = require("ethers");
+
+const mnemonic = "trigger short carbon offer combine camera shiver roast salmon unit another dampe";
+const wallet = ethers.Wallet.fromMnemonic(mnemonic);
+
+// console.log(`Mnemonic: ${wallet.mnemonic.phrase}`);
+console.log(`Address: ${youraddress}`);
+
+module.exports = wallet;
