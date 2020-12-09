0cfd697376353ca14fbc87469d8274ccdc425498
Lock3rDev <lock3r@programmer.net>
2020-12-09 09:45:08 +0000

    Delete lock3runitrade.js

--- abis/lock3runitrade.js
+++
@@ -1,8 +0,0 @@
-const config = {
-    address: "", //not ready
-    abi: [
-        "function work (  ) external",
-        "function workable (  ) external view returns ( bool )",
-    ],
-};
-module.exports = config;
